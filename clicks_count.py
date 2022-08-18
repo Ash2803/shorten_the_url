27742fa054134eb10096be48a0a47f48bf23bd52
Ashot Abazyan <ashot.93@mail.ru>
2022-08-18 23:46:15 +0300

    Added count_clicks function to count the number of clicks on url

--- clicks_count.py
+++ clicks_count.py
@@ -1,5 +1,6 @@
-import requests
 import os
+
+import requests
 from dotenv import load_dotenv
 
 
@@ -16,16 +17,32 @@ def shorten_link(token, url):
     return response.json()['link']
 
 
+def count_clicks(token, link):
+    bitlink = 'https://api-ssl.bitly.com/v4/shorten'
+    link = 'https://mail.ru/'
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+    params = {
+        'unit': 'day',
+        'units': '-1'
+    }
+    response = requests.post(bitlink, headers=headers, params=params)
+    response.raise_for_status()
+    return response.json()
+
+
 def main():
     load_dotenv()
     token = os.getenv('TOKEN')
     url = input("Enter the url: ")
+    link = 'https://mail.ru/'
     try:
         print('Битлинк', shorten_link(token, url))
     except requests.exceptions.HTTPError:
         print("Введен неправильный URL")
+    print(count_clicks(token, link))
 
 
 if __name__ == '__main__':
     main()
-
